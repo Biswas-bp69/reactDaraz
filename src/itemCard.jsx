d633d5a8e30cf41c756cf8fd563d570321e4137f
Biswas542 <biswasparajuli224@gmail.com>
2026-02-15 23:36:14 +0545

    new update

--- src/itemCard.jsx
+++ src/itemCard.jsx
@@ -7,7 +7,7 @@ function ItemCard() {
     <div className="itemImg"></div>
     <h3 className='mt-3 text-center'>Item Price Here</h3>
     <p>Lorem ipsum dolor  sit sit sit amet consectetur adipisicing elit. Quasi odio at Quasi odio at odio at autem, dolore reprehenderit corporis</p>
-    <button className='btn btn-primary w-100'>Add To Card</button>
+    <button className='btn btn-primary w-100'>Add To Cards</button>
 
   </div>
 }
