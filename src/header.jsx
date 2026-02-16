e20e833afe1fdbaf9b054cb8ef012812c6cb3b8e
Biswas542 <biswasparajuli224@gmail.com>
2026-02-16 09:01:30 +0545

    header update

--- src/header.jsx
+++ src/header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="container">
       <div className="row">
         <div className="col-lg-3">
-          <img src={logo} alt="logo" className='logo'/>
+          <img src={logo} alt="logo" className='logo' />
         </div>
         <div className="col-lg-5 d-flex justify-content-center align-items-center">
           <div className="searchArea">
@@ -15,7 +15,7 @@ function Header() {
           </div>
         </div>
         <div className="col-lg-4 d-flex justify-content-center align-items-center">
-          <nav>
+          <nav className='d-none d-lg-block'>
             <ul >
               <li><a href="#">Home</a></li>
               <li><a href="#">About</a></li>
@@ -26,7 +26,9 @@ function Header() {
           </nav>
 
         </div>
+
       </div>
+
     </div>
 
 
@@ -35,7 +37,16 @@ function Header() {
 
 
 
-
+    <details class="roleMenu d-block d-lg-none">
+      <summary class="dotBtn"><i class="fa-solid fa-bars text-white"></i>menu</summary>
+      <div class="dropdown">
+        <a href="#flashSale">Flash Sale</a>
+        <a href="#Categories"> Categories</a>
+        <a href="#forYou"> For You</a>
+        <a class="btn text-dark text-lg-light " data-bs-toggle="modal" data-bs-target="#contactModal"> Contact Us</a>
+        <a class="btn btn-primary text-light" data-bs-toggle="modal" data-bs-target="#registerModal"> Register </a>
+      </div>
+    </details>
 
   </header>
 }
